63571abf20365edd888ab3bf4bce8a36dae3370b
lfemina <lfemina@yandex.ru>
2024-10-06 23:52:17 +0300

    added markup, styles

--- src/presentation/App/App.js
+++ src/presentation/App/App.js
@@ -4,11 +4,45 @@ import { List } from "../List/List";
 import "./styles.sass";
 
 export function App() {
+  // контейнер с заголовками, вмещающий в себя форму, сообщение о загрузке, ответе и ошибке
   const app = document.createElement("div");
 
-  app.className = "app";
+  // создаём ещё один div, в нём будет заголовок h2
+  const titleForm =  document.createElement('div');
 
+  // создаём заголовки, помещаем их в контейнеры
+  const titleDoc = document.createElement('h1');
+  const titleFormH2 = document.createElement('h2');
+  titleForm.appendChild(titleFormH2);
+
+  ///создаём div для сообщения о загрузке данных с сервера - вроде тут лолжно быть
+  const loading = document.createElement('div');
+  ///создаём div для выведения ошибки запроса с сервера - вроде тут лолжно быть
+  const errorDiv = document.createElement('div');
+
+  //помещаем все элементы в container
+  app.appendChild(titleDoc);
+  app.appendChild(titleForm);
+  app.appendChild(loading); // если отдельно - добавить как функцию
+  app.appendChild(errorDiv); // если отдельно - добавить как функцию
   app.appendChild(Form());
+
+  // добавляем классы и контент
+  app.className = "container";
+  titleDoc.innerText = "Готовые рецепты для разнообразия вашего рациона"; // заголовок страницы
+  titleForm.className = 'title'; // контейнер для h2
+  titleFormH2.textContent = 'Не знаете что приготовить? Предлагаем Вам на выбор готовые рецепты!'; // h2
+  
+  // контейнер с сообщением о загрузке
+  loading.id = 'loading';
+  loading.className = 'reqItems';
+  loading.style.display = 'none';
+  loading.textContent = 'Идёт загрузка...';
+  
+  // контейнер с сообщением об ошибке
+  errorDiv.className = 'error';
+  errorDiv.id = 'error';
+  errorDiv.textContent = '';
 
   return app;
 }
--- src/presentation/Recipe/Recipe.js
+++ src/presentation/Recipe/Recipe.js
@@ -1,12 +1,20 @@
+import "./styles.sass";
+
 export function Recipe(recipe) {
   // функция отображает рецепт (принимает объект )
   const container = document.createElement("div"); // метод recipeDto
 
   container.innerHTML = `
-        <h2>${recipe.recipeDto.title}</h2> 
-        <p>${recipe.recipeDto.ingredient}</p>
-        <p>${recipe.recipeDto.body}</p>
+        <div class="header">
+          <img class="img" src="${recipe.recipeDto.image}">
+          <h2 class="title">${recipe.recipeDto.title}</h2> 
+        </div>
+        <p class="content">${recipe.recipeDto.ingredient}</p>
+        <p class="content">${recipe.recipeDto.body}</p>
     `;
+
+  // добавляем классы
+  container.className = 'result';
 
   return container;
 }
